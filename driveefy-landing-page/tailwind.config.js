89f8e335f0b89fa88a2d9119a703d1ec46d10d0e
dharmilshiroyadriveefy <dharmilshiroyadriveefy@gmail.com>
2025-01-25 10:56:56 +0530

    Delete driveefy-landing-page directory
    updated-new repository with folder name

--- driveefy-landing-page/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-export const content = [
-  './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths according to your project structure
-];
-export const theme = {
-  extend: {
-    // Add custom classes here if needed
-  },
-};
-export const plugins = [];
